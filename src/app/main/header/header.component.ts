b8d94083552a96a06c3438618622f694eb42ce95
Daniel Kerwin <dkerwin@liveperson.com>
2017-11-24 01:06:53 +0000

    feat: modal opens/closes on login/logout

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApiLoginService } from '../../shared/services/api-login.service';
 import { ModalComponent } from '../modal/modal.component';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   dialogRef: MatDialogRef<ModalComponent>;
+  eventsSub: Subscription;
 
   constructor(public dialog: MatDialog, private apiLoginService: ApiLoginService) {}
 
@@ -17,14 +19,29 @@ export class HeaderComponent implements OnInit {
    * Opens the material dialog modal
    */
   openDialog(): void {
-    this.dialogRef = this.dialog.open(ModalComponent, { maxWidth: 400 });
+    this.dialogRef = this.dialog.open(ModalComponent, {
+      maxWidth: 400,
+      position: { top: '5%', right: '5%' }
+    });
   }
 
   ngOnInit() {
-    // open dialog on login if no token found
-    // timeout required due to known angular bug with opening dialog during change detection
-    if (!this.apiLoginService.bearer) {
-      setTimeout(() => this.openDialog(), 100);
-    }
+    // sub to events and open dialog if not logged in
+    this.eventsSub = this.apiLoginService.events$.subscribe(event => {
+      if (event.isLoggedIn) {
+        if (this.dialogRef) {
+          this.dialogRef.close();
+        }
+      } else {
+        if (!this.dialogRef) {
+          // timeout required due to known angular bug with opening dialog during change detection
+          setTimeout(() => this.openDialog(), 100);
+        }
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.eventsSub.unsubscribe();
   }
 }
