bb6f33957c5a1adb84b640a77702b1fe36ed7330
Daniel Kerwin <dkerwin@liveperson.com>
2018-02-27 20:04:36 +0000

    feat(AssessmentsGrid): templatise column assignment per grid type

--- src/app/main/assessments/assessments-grid/assessments-grid.component.ts
+++ src/app/main/assessments/assessments-grid/assessments-grid.component.ts
@@ -1,38 +1,71 @@
-import { Component, OnInit, Input, ViewChild, HostBinding } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  HostBinding,
+  TemplateRef,
+} from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { StoreModel } from '../../../app.store';
 
-import { DatatableComponent } from '@swimlane/ngx-datatable';
+import { DatatableComponent, TableColumn } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-assessments-grid',
   templateUrl: './assessments-grid.component.html',
-  styleUrls: ['./assessments-grid.component.css']
+  styleUrls: ['./assessments-grid.component.css'],
 })
 export class AssessmentsGridComponent implements OnInit {
   @HostBinding('class') class = 'col';
   @ViewChild('table') table: DatatableComponent;
+  @ViewChild('checkCell') checkCell: TemplateRef<any>;
+  @ViewChild('checkHeader') checkHeader: TemplateRef<any>;
   @Input() data: any[];
   @Input() type: string;
 
   selected = [];
-  columns = [];
+  columns: TableColumn[] = [];
   rows = [];
 
-  constructor(private store: Store<StoreModel>) { }
+  constructor(private store: Store<StoreModel>) {}
 
   onSelect({ selected }) {
     this.selected.splice(0, this.selected.length);
     this.selected = [...selected];
-    // this.store.dispatch(new AfDataActions.SelectDocument(this.type, this.selected));
   }
 
-  ngOnInit() {
+  /**
+   * returns columns for data grid depending on type of grid
+   * @return {TableColumn[]}
+   */
+  getColums(): TableColumn[] {
+    const common = {
+      cellTemplate: this.checkCell,
+      headerTemplate: this.checkHeader,
+      width: 30,
+    };
+    let columns = [];
+    if (this.type === 'playlists') {
+      columns = [
+        common,
+        { prop: 'createdBy', name: 'By', cellClass: 'datatable-cells' },
+        { prop: 'createdAt', name: 'Created', cellClass: 'datatable-cells' },
+        { prop: 'id', name: 'ID' },
+      ];
+    } else {
+      columns = [
+        common,
+        { prop: 'createdBy', name: 'By', cellClass: 'datatable-cells' },
+        { prop: 'createdAt', name: 'Created', cellClass: 'datatable-cells' },
+        { prop: 'id', name: 'ID' },
+      ];
+    }
+    return columns;
+  }
+
+  ngOnInit(): void {
     this.rows = this.data;
-    this.columns = [
-      { prop: 'createdBy', name: 'Created', cellClass: 'datatable-cells' },
-      { prop: 'id', name: 'ID' },
-    ];
+    this.columns = this.getColums();
   }
-
 }
