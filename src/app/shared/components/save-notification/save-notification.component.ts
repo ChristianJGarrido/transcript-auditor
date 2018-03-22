bba05f0cff01286d1e3199993cc2d715f23bade7
Daniel Kerwin <dkerwin@liveperson.com>
2018-03-22 01:21:27 +0000

    fix(SaveNotification): changeDetection not firing due to onPush strategy

--- src/app/shared/components/save-notification/save-notification.component.ts
+++ src/app/shared/components/save-notification/save-notification.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, ChangeDetectorRef } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-save-notification',
   templateUrl: './save-notification.component.html',
-  styleUrls: ['./save-notification.component.css']
+  styleUrls: ['./save-notification.component.css'],
 })
 export class SaveNotificationComponent implements OnChanges {
   @Input() updating: boolean = null;
 
-  constructor() { }
+  constructor(private changeDetector: ChangeDetectorRef) {}
 
   ngOnChanges() {
-    setTimeout(() => this.updating = !this.updating ? null : this.updating, 1000);
+    setTimeout(() => {
+      this.updating = !this.updating ? null : this.updating;
+      this.changeDetector.detectChanges();
+    }, 1000);
   }
-
 }
