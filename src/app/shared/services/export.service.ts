a2e2f44f9bf3c6b81b52009b9ea9c4c7de2b2ae2
Daniel Kerwin <dkerwin@liveperson.com>
2018-04-16 13:26:02 +0100

    feat(ExportService): allow adding data to existing worksheet of a xlsx template

--- src/app/shared/services/export.service.ts
+++ src/app/shared/services/export.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { switchMap } from 'rxjs/operators/switchMap';
 import { AssessmentModel } from '../store/assessment/assessment.model';
 import { PlaylistModel } from '../store/playlist/playlist.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import * as papaparse from 'papaparse';
 import * as _ from 'lodash';
@@ -11,7 +12,7 @@ import * as XLSX from 'xlsx';
 
 @Injectable()
 export class ExportService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   /**
    * take collection of assessments or playlists
@@ -36,6 +37,16 @@ export class ExportService {
     XLSX.writeFile(wb, `${name}-${date}.xlsx`);
   }
 
+  // test adding data to existing worksheet
+  getTemplate(rows) {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    this.http.get('/assets/template.xlsx', { headers, responseType: 'arraybuffer' }).subscribe(res => {
+      const wb: XLSX.WorkBook = XLSX.read(res, { type: 'array' });
+      const ws: XLSX.WorkSheet = wb.Sheets['Data'];
+      XLSX.utils.sheet_add_json(ws, rows);
+      XLSX.writeFile(wb, `Test.xlsx`);
+    }, error => console.log(error));
+  }
 
   /**
    * export data to csv
